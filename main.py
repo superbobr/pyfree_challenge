089710045822942c257ee44db15e677f8256af9a
superbobr <42520236+superbobr@users.noreply.github.com>
2021-07-15 10:39:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+#task3
+todo = {}
+user_date1 = input("Введите дату:")
+task1 = input("Введите задачу:")
+todo[user_date1] = task1
+user_date2 = input("Введите дату:")
+task2 = input("Введите задачу:")
+todo[user_date2] = task2
+user_date3 = input("Введите дату:")
+task3 = input("Введите задачу:")
+todo[user_date3] = task3
+
+#task2
+#user_date1 = input("Введите дату:")
+#task1 = input("Введите задачу:")
+#user_date2 = input("Введите дату:")
+#task2 = input("Введите задачу:")
+#user_date3 = input("Введите дату:")
+#task3 = input("Введите задачу:")
+#print(user_date1, task1)
+#print(user_date2, task2)
+#print(user_date3, task3)
+
+#task1
+#user_date = input("Введите дату:")
+#task = input("Введите задачу:")
+#print(user_date, task)
